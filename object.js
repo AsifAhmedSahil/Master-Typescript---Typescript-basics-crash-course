38cb81a601556db7be306c4837577a1d4f1dd24e
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-04-30 15:12:55 +0600

    objects

---
+++ object.js
@@ -0,0 +1,9 @@
+var student = {
+    name: "sahil",
+    address: "chittagong",
+    phone: 1235689,
+    isActive: true
+};
+console.log(student);
+console.log(student.name);
+console.log(student.phone);
---
+++ object.ts
@@ -0,0 +1,16 @@
+let student:{
+    name: string,
+    address: string,
+    phone: number,
+    isActive: boolean
+} = {
+    name: "sahil",
+    address: "chittagong",
+    phone:1235689,
+    isActive: true
+
+}
+
+console.log(student)
+console.log(student.name)
+console.log(student.phone)
