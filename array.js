246bf1a0aca8d422b5dc475527ce39279cfbe0d5
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-04-30 14:52:27 +0600

    array complete

--- array.js
+++ array.js
@@ -5,3 +5,30 @@ console.log(numbers);
 var friends;
 friends = ["sahil", "asif", "ahmed"];
 console.log(friends);
+var newLength = friends.push("sanjida");
+console.log(newLength);
+console.log(friends);
+console.log(friends.join("---"));
+// contextual typing ****
+// contextual typing means friend er modde data ache aguli string type tai value ta o string type holo**
+friends.forEach(function (value) {
+    console.log(value.toLocaleUpperCase());
+});
+var newFriendArray = friends.map(function (value) {
+    return value.toUpperCase();
+});
+console.log(newFriendArray);
+// -----------------------mixed type array -----------------
+var mixed;
+mixed = ["sahil", "asif", 45, 25];
+mixed.forEach(function (value) {
+    if (typeof value === "string") {
+        var uppercase = value.toUpperCase();
+        console.log(uppercase);
+    }
+    else {
+        var number = value.toFixed(2);
+        console.log(number);
+    }
+});
+console.log("mixed array is:", mixed);
--- array.ts
+++ array.ts
@@ -7,3 +7,43 @@ console.log(numbers)
 let friends:string[];
 friends = ["sahil","asif","ahmed"]
 console.log(friends)
+
+let newLength: number = friends.push("sanjida")
+console.log(newLength)
+console.log(friends)
+console.log(friends.join("---"))
+
+// contextual typing ****
+
+// contextual typing means friend er modde data ache aguli string type tai value ta o string type holo**
+
+friends.forEach((value) =>{
+    console.log(value.toLocaleUpperCase())
+
+})
+
+let newFriendArray = friends.map((value) =>{
+    return value.toUpperCase()
+})
+
+
+console.log(newFriendArray)
+
+// -----------------------mixed type array -----------------
+
+let mixed: (string | number)[];
+
+ mixed = ["sahil","asif",45,25]
+
+ mixed.forEach((value) =>{
+    if(typeof value === "string"){
+        let uppercase = value.toUpperCase()
+        console.log(uppercase)
+    }
+    else{
+        let number = value.toFixed(2)
+        console.log(number)
+    }
+ })
+
+ console.log("mixed array is:",mixed)
